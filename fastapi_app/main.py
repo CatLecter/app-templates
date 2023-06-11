18964949eed43cac79f2844068b3b0e0688c7b73
Vasilii Safronov <v.safronov@toolstrek.com>
2023-06-11 14:20:05 +0300

    FastAPI abandoned DI and Request.state in favor of performance

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,42 +1,19 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from db.postgres import PostgresDB
 from routers import router
 from settings import settings
 
+app = FastAPI(
+    title='FastAPI',
+    version='1.0',
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
 
-def create_app() -> FastAPI:
-    application = FastAPI(
-        title='FastAPI',
-        version='1.0',
-        docs_url='/api/openapi',
-        openapi_url='/api/openapi.json',
-        default_response_class=ORJSONResponse,
-    )
-
-    db = PostgresDB()
-
-    @application.on_event("startup")
-    async def startup():
-        await db.create_pool()
-
-    @application.on_event("shutdown")
-    async def shutdown():
-        await db.close_pool()
-
-    @application.middleware("http")
-    async def db_session_middleware(request: Request, call_next):
-        request.state.db = db
-        return await call_next(request)
-
-    application.include_router(router)
-
-    return application
-
-
-app = create_app()
+app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run(
--- fastapi_app/routers/users.py
+++ fastapi_app/routers/users.py
@@ -1,33 +1,36 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import UUID4
 
+from db.postgres import PostgresDB
 from schemes.users import ResponseUser, User
 
 router = APIRouter(prefix='/user', tags=['Users'])
 
+db = PostgresDB()
+
 
 @router.get(path='/{user_id}', response_model=ResponseUser)
-async def get_user(user_id: UUID4, request: Request) -> ResponseUser:
-    user = await request.state.db.get_user(user_id=user_id)
+async def get_user(user_id: UUID4) -> ResponseUser:
+    user = await db.get_user(user_id=user_id)
     if not user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'User with UUID={user_id} not found')
     return user
 
 
 @router.post(path='/', response_model=ResponseUser)
-async def post_user(item: User, request: Request) -> ResponseUser:
-    user = await request.state.db.add_user(user=item)
+async def post_user(item: User) -> ResponseUser:
+    user = await db.add_user(user=item)
     if not user:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f'Failed to create user')
     return user
 
 
 @router.put(path='/{user_id}', response_model=ResponseUser)
-async def put_user(user_id: UUID4, item: User, request: Request) -> ResponseUser:
-    user = await request.state.db.update_user(user=item, user_id=user_id)
+async def put_user(user_id: UUID4, item: User) -> ResponseUser:
+    user = await db.update_user(user=item, user_id=user_id)
     if not user:
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
@@ -37,8 +40,8 @@ async def put_user(user_id: UUID4, item: User, request: Request) -> ResponseUser
 
 
 @router.delete(path='/{user_id}', response_class=JSONResponse)
-async def delete_user(user_id: UUID4, request: Request) -> JSONResponse:
-    id_deleted_user = await request.state.db.delete_user(user_id=user_id)
+async def delete_user(user_id: UUID4) -> JSONResponse:
+    id_deleted_user = await db.delete_user(user_id=user_id)
     if not id_deleted_user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'User with UUID={user_id} not found')
     return JSONResponse(content={'result': 'successful', 'message': f'User with UUID={id_deleted_user}'})
